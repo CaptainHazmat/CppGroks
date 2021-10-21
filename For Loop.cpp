3030ab5c5b03faea1c6510eb7f1a18e19f331b04
Azatbek <72803057+Hazmat03@users.noreply.github.com>
2021-10-21 17:07:03 +0600

    Add files via upload

---
+++ For Loop.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	int a, b, n, g;
+	cin >> a;
+	cin >> b;
+	n = a--;
+	const int size = 10;
+	string ar[size] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	for (n = a; n < b; n++)
+	{
+		if (n >= 9) {
+			if (n % 2 == 0) {
+				cout << "odd" << endl;
+			}
+			else {
+				cout << "even" << endl;
+			}
+		}
+		else {
+			cout << ar[n] << endl;
+		}
+	}
+	return 0;
+}
