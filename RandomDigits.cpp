0fd4025f9c98c3baa77247db341f5b2d7e5a8332
Azatbek <72803057+Hazmat03@users.noreply.github.com>
2021-10-27 12:09:33 +0600

    Update RandomDigits.cpp

--- RandomDigits.cpp
+++ RandomDigits.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>	// contains rand(), srand()
 #include <ctime> // to randomize digits (time)
+#include <windows.h> // to delay running
 using namespace std;
 
 int main()
@@ -10,6 +11,7 @@ int main()
 	cout << "Enemy uses Battle axe\n";
 	srand(time(NULL));
 	dmg = rand() % 20;	// in the range up to 20
+	Sleep(1000); // delay (thousandth of a second)
 	cout << "Enemy damaged you to -" << dmg << " HP\n";
 	return 0;
 }
