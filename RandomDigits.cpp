106adcdb3629b812feea1a90ddf452ad69c89913
Azatbek <72803057+Hazmat03@users.noreply.github.com>
2021-10-27 11:56:54 +0600

    Update RandomDigits.cpp

--- RandomDigits.cpp
+++ RandomDigits.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-	int a = 0;
+	int dmg;
 	cout << "Enemy appears" << endl;
 	cout << "Enemy uses Battle axe\n";
 	srand(time(NULL));
-	a = rand() % 20;	// in the range up to 20
-	cout << "Enemy damaged you to -" << a << " HP\n";
+	dmg = rand() % 20;	// in the range up to 20
+	cout << "Enemy damaged you to -" << dmg << " HP\n";
 	return 0;
 }
