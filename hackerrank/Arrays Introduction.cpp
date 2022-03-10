9a4d2802619d40f153d4376bee043447a407bdcc
Azatbek <72803057+Hazmat03@users.noreply.github.com>
2022-03-10 09:37:04 +0600

    Update Arrays Introduction.cpp

--- hackerrank/Arrays Introduction.cpp
+++ hackerrank/Arrays Introduction.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 int main()
 {
-    int n, h;
+    int n;
     cin >> n;
     int* arr = new int[n];
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        h = arr[i];
     }
     for (int j = n - 1; j >= 0; j--) {
         cout << arr[j] << " ";
